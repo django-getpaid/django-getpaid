0c848244beeab1203bd274f0a5fcdffbd3694852
Krzysztof Dorosz <cypreess@gmail.com>
2012-08-06 12:15:54 +0200

    Fixing problem with PyPI packaging, bumping to 1.1.1.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
-with open('README.rst') as file:
+with open('README.txt') as file:
     long_description = file.read()
 
 setup(
     name='django-getpaid',
     description='Multi-broker payment processor for django',
     long_description=long_description,
-    version='1.1',
-    packages=['getpaid',],
+    version='1.1.1',
+    packages=find_packages(),
     url='https://github.com/cypreess/django-getpaid',
     license='MIT',
     author='Krzysztof Dorosz',
