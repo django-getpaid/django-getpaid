ecade49b263fac86268baeb5f9328afcee098b28
Krzysztof Dorosz <cypreess@gmail.com>
2012-11-20 16:38:29 +0100

    Bumping to unstable version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='django-getpaid',
     description='Multi-broker payment processor for django',
     long_description=long_description,
-    version='1.3.0',
+    version='1.3.0dev',
     packages=find_packages(),
     url='https://github.com/cypreess/django-getpaid',
     license='MIT',
