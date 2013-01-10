71a1a7a3bd2d6c78975906c8cde0198bc03cb0bf
Krzysztof Dorosz <cypreess@gmail.com>
2013-01-11 00:03:14 +0100

    Changing order admin field in payments to raw id

--- getpaid/admin.py
+++ getpaid/admin.py
@@ -7,5 +7,7 @@ class PaymentAdmin(admin.ModelAdmin):
     list_display = ('id', 'amount', 'currency', 'status', 'created_on', 'paid_on', 'amount_paid' )
     list_filter = ( 'status', 'created_on', 'paid_on')
     search_fields = ('id', )
+    raw_id_fields = ('order', )
+
 
 admin.site.register(Payment, PaymentAdmin)
