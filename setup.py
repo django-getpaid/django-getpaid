ad3c284596f9cdb3114cb058c5557fa85626ffdf
Krzysztof Dorosz <cypreess@gmail.com>
2013-05-11 19:34:30 +0200

    Version 1.5.0

--- setup.py
+++ setup.py
@@ -1,50 +1,28 @@
 from setuptools import find_packages, setup
 
-with open('README.rst') as file:
-    long_description = file.read()
+with open('README.rst') as f:
+    long_description = f.read()
 
 setup(
     name='django-getpaid',
     description='Multi-broker payment processor for django',
     long_description=long_description,
-    version='1.5.0dev',
+    version='1.5.0',
     packages=find_packages(),
     url='https://github.com/cypreess/django-getpaid',
     license='MIT',
     author='Krzysztof Dorosz',
     author_email='cypreess@gmail.com',
-    extras_require = {
+    extras_require={
         'payu': [
             'django-celery>=3.0.11',
         ],
+        'przelewy24': [
+            'django-celery>=3.0.11',
+        ],
         'moip': [
-        	'requests',
+            'requests',
         ],
     },
 
-    package_data={
-        'getpaid': [
-            'templates/getpaid/*.html',
-            'locale/pl/LC_MESSAGES/*',
-            'locale/pt_BR/LC_MESSAGES/*',
-        ],
-        'getpaid.backends.dummy': [
-            'templates/getpaid_dummy_backend/*.html',
-        ],
-
-        'getpaid.backends.dotpay': [
-            'static/getpaid/backends/dotpay/*',
-        ],
-
-        'getpaid.backends.payu': [
-            'static/getpaid/backends/payu/*',
-        ],
-
-        'getpaid.backends.transferuj': [
-            'static/getpaid/backends/transferuj/*',
-        ],
-        'getpaid.backends.moip': [
-            'static/getpaid/backends/moip/*',
-            ],
-    },
 )
