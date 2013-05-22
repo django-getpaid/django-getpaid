f08a3288e3d1798c04c27acb9ba0e13d0c21b964
mko_san@lafiel.net <mononoke_hime_85>
2013-05-22 11:36:12 +0200

    typo fix

--- getpaid/backends/skrill/views.py
+++ getpaid/backends/skrill/views.py
@@ -21,9 +21,9 @@ class OnlineView(View):
             merchant_id = request.POST['merchant_id']
             transaction_id = request.POST['transaction_id']
             mb_transaction_id = request.POST['mb_transaction_id']
-            mb_amount = request.POST['mb_amount']
+            mb_amount = request.POST['mb_amoun
             mb_currency = request.POST['mb_currency']
-            status = request.POST['status']
+            status = int(request.POST['status'])
             sig = request.POST['md5sig']
             pay_from_email = request.POST['pay_from_email']
         except KeyError:
