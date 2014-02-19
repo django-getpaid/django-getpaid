10279762e688f008b69469e49296d80d1fda8f76
Krzysztof Dorosz <cypreess@gmail.com>
2014-02-19 19:09:44 +0100

    code reformat

--- getpaid_test_project/getpaid_test_project/settings_test.py
+++ getpaid_test_project/getpaid_test_project/settings_test.py
@@ -1,29 +1,29 @@
 #noinspection PyUnresolvedReferences
 from settings import *
 
-GETPAID_BACKENDS = ('getpaid.backends.dummy',
-                    'getpaid.backends.payu',
-                    'getpaid.backends.transferuj',
-                    'getpaid.backends.przelewy24',
-                    )
+GETPAID_BACKENDS = (
+    'getpaid.backends.dummy',
+    'getpaid.backends.payu',
+    'getpaid.backends.transferuj',
+    'getpaid.backends.przelewy24',
+)
 
-
-INSTALLED_APPS += ('getpaid.backends.payu', 'getpaid.backends.transferuj',)
+INSTALLED_APPS += GETPAID_BACKENDS
 
 GETPAID_BACKENDS_SETTINGS = {
     # Please provide your settings for backends
-    'getpaid.backends.payu' : {
-        'pos_id' : 123456789,
-        'key1' : 'xxx',
-        'key2' : 'xxx',
+    'getpaid.backends.payu': {
+        'pos_id': 123456789,
+        'key1': 'xxx',
+        'key2': 'xxx',
         'pos_auth_key': 'xxx',
-        'signing' : True,
+        'signing': True,
         #        'testing' : True,
     },
 
-    'getpaid.backends.transferuj' : {
-        'id' : 1234,
-        'key' : 'AAAAAAAA',
+    'getpaid.backends.transferuj': {
+        'id': 1234,
+        'key': 'AAAAAAAA',
 
     },
 
