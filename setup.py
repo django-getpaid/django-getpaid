29c38bad0e139fa5d028f0fdd7c35ec145d6faaa
pawciobiel <pawciobiel@gmail.com>
2015-11-17 00:42:46 +0100

    version 1.7.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='django-getpaid',
     description='Multi-broker payment processor for django',
     long_description=long_description,
-    version='1.7.2',
+    version='1.7.3',
     packages=find_packages(),
     url='https://github.com/cypreess/django-getpaid',
     license='MIT',
