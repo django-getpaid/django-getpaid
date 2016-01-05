96ac613d265b28bcae3a7cf902ffa5ef03df18f2
Tomek Główka <tk.glowka@gmail.com>
2016-01-05 22:57:50 +0100

    Proxied import of register_to_payment

--- getpaid/__init__.py
+++ getpaid/__init__.py
@@ -1,4 +1,9 @@
-#noinspection PyUnresolvedReferences
-from .models import register_to_payment
-
 default_app_config = 'getpaid.apps.Config'
+
+
+def register_to_payment(*args, **kwargs):
+    """
+    Thin proxy for actual register_to_payment to prevent uncontrolled early loading of models directory.
+    """
+    from . import models
+    return models.register_to_payment(*args, **kwargs)
