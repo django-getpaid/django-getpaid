cdd00849e5b79b1d47bfde7f030a1d0715b3005c
Łukasz Banasiak <lukas.banasiak@gmail.com>
2017-05-20 01:19:04 +0200

    Add support for Django 1.11
    - remove usage undocumented classes that was removed in Django 1.11
    https://docs.djangoproject.com/en/1.11/releases/1.11/#template-widget-incompatibilities-1-11

--- getpaid/forms.py
+++ getpaid/forms.py
@@ -1,43 +1,12 @@
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.forms import forms
 from django.forms.fields import ChoiceField, CharField
 from django.forms.models import ModelChoiceField
-from django.forms.widgets import HiddenInput, RadioSelect, RadioFieldRenderer, RadioChoiceInput
-
-from django.utils.encoding import force_text
-
-from django.utils.safestring import mark_safe
+from django.forms.widgets import HiddenInput, RadioSelect
 from django.utils.translation import ugettext as _
+
 from getpaid.models import Order
-from .utils import get_backend_choices, import_name
-
-
-class PaymentRadioInput(RadioChoiceInput):
-    def __init__(self, name, value, attrs, choice, index):
-        super(PaymentRadioInput, self).__init__(name, value, attrs, choice, index)
-        logo_url = import_name(choice[0]).PaymentProcessor.get_logo_url()
-        if logo_url:
-            self.choice_label = mark_safe('<img src="%s%s" alt="%s">' % (
-                getattr(settings, 'STATIC_URL', ''),
-                logo_url,
-                force_text(choice[1]),
-                )
-            )
-
-
-class PaymentRadioFieldRenderer(RadioFieldRenderer):
-    def __iter__(self):
-        for i, choice in enumerate(self.choices):
-            yield PaymentRadioInput(self.name, self.value, self.attrs.copy(), choice, i)
-
-    def __getitem__(self, idx):
-        choice = self.choices[idx]  # Let the IndexError propogate
-        return PaymentRadioInput(self.name, self.value, self.attrs.copy(), choice, idx)
-
-
-class PaymentRadioSelect(RadioSelect):
-    renderer = PaymentRadioFieldRenderer
+from .utils import get_backend_choices
 
 
 class PaymentMethodForm(forms.Form):
@@ -52,7 +21,7 @@ class PaymentMethodForm(forms.Form):
             choices=backends,
             initial=backends[0][0] if len(backends) else '',
             label=_("Payment method"),
-            widget=PaymentRadioSelect,
+            widget=RadioSelect,
         )
 
     order = ModelChoiceField(widget=HiddenInput, queryset=Order.objects.all())
