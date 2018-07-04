65a2b610e8e4f5ceb8a48fe8ae52c486e502ae81
Dominik Kozaczko <dominik@kozaczko.info>
2018-07-04 11:05:07 +0200

    Bump version, update docs, prepare for 1.8.0 release

--- getpaid/__init__.py
+++ getpaid/__init__.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 __version__ = '1.8.0-rc1'
-=======
-__version__ = '2.0.0'
->>>>>>> Redesign project structure according to cookiecutter-djangopackage
+
 default_app_config = 'getpaid.apps.GetpaidConfig'
 
 
