ab03e13ef662ba2ef6be85fde8a60441fd5b91ad
Dominik Kozaczko <dominik@kozaczko.info>
2018-07-04 07:02:58 +0200

    Fix compatibility with Django 2.0 on example project and tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,6 @@
 # noinspection PyUnresolvedReferences
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
