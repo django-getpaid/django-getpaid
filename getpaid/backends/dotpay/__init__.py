e838815722bb9864e35dae9e52278ad8ca871487
anih <admin@e-back.net>
2018-07-04 13:14:45 +0200

    put proper dotpay gateway link (#112)
    Previous link was wrong and send client to website with broken css/js.
    As with documentation we should use https://ssl.dotpay.pl/t2/

--- getpaid/backends/dotpay/__init__.py
+++ getpaid/backends/dotpay/__init__.py
@@ -31,10 +31,8 @@ class PaymentProcessor(PaymentProcessorBase):
 
     _ALLOWED_IP = ('195.150.9.37',)
     _ACCEPTED_LANGS = ('pl', 'en', 'de', 'it', 'fr', 'es', 'cz', 'ru', 'bg')
-    _GATEWAY_URL = 'https://ssl.dotpay.eu/'
-    _ONLINE_SIG_FIELDS = (
-        'id', 'control', 't_id', 'amount', 'email', 'service', 'code', 'username', 'password', 't_status'
-    )
+    _GATEWAY_URL = 'https://ssl.dotpay.pl/t2/'
+    _ONLINE_SIG_FIELDS = ('id', 'control', 't_id', 'amount', 'email', 'service', 'code', 'username', 'password', 't_status')
 
     @staticmethod
     def compute_sig(params, fields, PIN):
