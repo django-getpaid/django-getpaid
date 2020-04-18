941ed1800186547bb12027561cc300bd2c918fb4
Dominik Kozaczko <dominik@kozaczko.info>
2020-04-19 00:38:40 +0200

    Fix example app

--- example/orders/signals.py
+++ example/orders/signals.py
@@ -1,21 +1,19 @@
 import logging
 
 from django.dispatch import receiver
+from django_fsm.signals import post_transition
 
 from getpaid import PaymentStatus
-from getpaid.signals import payment_status_changed
 
 logger = logging.getLogger("getpaid_example")
 
 
-@receiver(payment_status_changed)
-def payment_status_changed_listener(sender, instance, old_status, new_status, **kwargs):
+@receiver(post_transition)
+def payment_status_changed_listener(sender, instance, name, source, target, **kwargs):
     """
     You can do something when payment is accepted. Let's change order status.
     """
-    logger.debug(
-        "payment_status_changed_listener, old=%s, new=%s", old_status, new_status
-    )
-    if new_status == PaymentStatus.PAID:
+    logger.debug("payment_status_changed_listener, old=%s, new=%s", source, target)
+    if target == PaymentStatus.PAID:
         instance.order.status = "P"
         instance.order.save()
